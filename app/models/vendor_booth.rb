7507ac7f1c352f3128e5d58558d6dee8c6779967
mmhayase <malloryhayase@gmail.com>
2015-10-07 13:20:14 -0400

    Added Validations and Scopes
    Added the following scopes:
    
    Vendor - alphabetical, by_tag(tag)
    Convention - alphabetical, ongoing, upcoming
    Vendor_Booth - chronological
    
    Added following validations:
    
    Tag - validates presence of tag
    Vendor_Booth - validates presence of start_time, end_time

--- app/models/vendor_booth.rb
+++ app/models/vendor_booth.rb
@@ -2,4 +2,10 @@ class VendorBooth < ActiveRecord::Base
     # Relationships
     belongs_to :vendor
     belongs_to :booth
+
+    # Validations
+    validates_presence_of :start_time, :end_time
+
+    # Scopes
+    scope :chronological, -> {order('start_time')}
 end
