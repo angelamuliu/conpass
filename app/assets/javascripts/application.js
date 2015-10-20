0686d714de8c023fdd07f483d8d98109c23fb7ea
Angela Liu <angelamuliu@gmail.com>
2015-10-20 18:11:02 -0400

    Added zoom in and out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,36 +48,48 @@ $(document).ready(function() {
 
 
 
-
+// ------------------------------------------------
 
 // Probably should move this later
 
 // USED ON: MAP SHOW PAGE
-// Given the x,y coords of topleft point of booth and width and height,
 // centers view on the booth and highlights it too
-function centerOnBooth(x, y, width, height, id) {
+function centerOnBooth() {
     $(".booth").removeClass("highlight");
     $("ul.vendorBooth").slideUp();
-    var left = -x - 0.5 * width + 0.5 * (window.innerWidth);
-    var top = -y;
+
+    var booth = $(this);
+    center(booth);
+
+    $(this).addClass("highlight");
+    $(this).children("ul.vendorBooth").slideDown();
+}
+function center(boothEl) {
+    var boothX = parseInt(boothEl.css("left"));
+    var boothY = parseInt(boothEl.css("top"));
+    var boothWidth = parseInt(boothEl.css("width"));
+
+    var left = -boothX - 0.5 * boothWidth + 0.5 * (window.innerWidth);
+    var top = -boothY;
 
     $("#mapArea").css("left", left);
     $("#mapArea").css("top", top);
-    $(".booth[data-id="+id+"]").addClass("highlight");
-    $(".booth[data-id="+id+"] ul.vendorBooth").slideDown();
 }
 function closeVendorBooth(event) {
     event.stopPropagation();
     $("ul.vendorBooth").slideUp();
 }
+$(document).ready(function() {
+    $(".booth").click(centerOnBooth);
+    // $(".booth").on("click touchstart", centerOnBooth);
 
+})
 
-
-
-
+// ------------------------------------------------
 
 // Should definitely move this elsewhere sometime
 
+// ZOOMING IN/OUT
 var zoomOptions = [0.5, 1, 2, 4];
 var zoomIndex = 1; // Refers to index in array above, 1 => 1
 
@@ -85,18 +97,41 @@ function zoomIn() {
     if (zoomIndex < zoomOptions.length - 1) {
         zoomIndex++;
         var zoomFactor = zoomOptions[zoomIndex] / zoomOptions[zoomIndex - 1];
-
-        // First zoom in map area
-        var mapWidth = $("#mapArea").css("width");
-        var mapHeight = $("#mapArea").css("height");
-        $("#mapArea").css("width", mapWidth * zoomFactor);
-        $("#mapArea").css("height", mapHeight * zoomFactor);
-
+        zoom(zoomFactor);
     }
 }
 function zoomOut() {
     if (zoomIndex > 0) {
         zoomIndex--;
+        var zoomFactor = zoomOptions[zoomIndex] / zoomOptions[zoomIndex + 1];
+        zoom(zoomFactor);
+    }
+}
+function zoom(zoomFactor) {
+    // First zoom the map
+    var mapWidth = parseInt($("#mapArea").css("width"));
+    var mapHeight = parseInt($("#mapArea").css("height"));
+    $("#mapArea").css("width", mapWidth * zoomFactor);
+    $("#mapArea").css("height", mapHeight * zoomFactor);
+
+    // Zoom the booths
+    var booths = $(".booth");
+    for (var i = 0; i < booths.length; i++) {
+        var booth = $(booths[i]);
+        var boothX = parseInt(booth.css("left"));
+        var boothY = parseInt(booth.css("top"));
+        var boothWidth = parseInt(booth.css("width"));
+        var boothHeight = parseInt(booth.css("height"));
+
+        booth.css("left", boothX * zoomFactor);
+        booth.css("top", boothY * zoomFactor);
+        booth.css("width", boothWidth * zoomFactor);
+        booth.css("height", boothHeight * zoomFactor);
+    }
+
+    // Recenter on the highlighted booth if any
+    if ($(".booth.highlight").length > 0) {
+        center($(".booth.highlight").first());
     }
 }
 
