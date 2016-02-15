9f6f261cd3423dde9c7a0ead4bad662a1ff996e4
Angela Liu <angelamuliu@gmail.com>
2016-02-15 15:36:03 -0500

    Restyled nav bar. Refactoring CSS into shared scss variables and by font, button, etc. Wrote script that creates typing effect on index. Started rebuilding index

--- app/assets/javascripts/global/home.js
+++ app/assets/javascripts/global/home.js
@@ -1,17 +1,60 @@
-// Listen to scroll to change navbar opacity
-function checkScroll() {
-  var startY = $('.navbar').height() * 2; // Line the navbar changes (in px)
 
-  if ($(window).scrollTop() > startY) {
-    $('.navbar').addClass("scrolled");
-  } else{
-    $('.navbar').removeClass("scrolled");
-  }
-}
+
+
+// The typing effect on the index page
 $(document).ready(function() {
-  if ($('.navbar').length > 0) {
-    $(window).on("scroll load resize", function() {
-      checkScroll();
-    });
-  }
-});
+
+    var flashLineListener;
+    var words = ["conventions", "festivals", "fairs", "conferences"];
+    var currWord = 0;
+
+    if ($("#introBanner .introContainer h1").size() > 0) {
+        flashLineListener = setInterval(flashLine, 600);
+        changeWord();
+    }
+
+    // Flashes the line located at the end of the H1 intro
+    function flashLine() {
+        var line = $("#introBanner .introContainer em");
+        if (line.css("opacity") == 0) {
+            line.css("opacity", 1);
+        } else { line.css("opacity", 0); }
+    }
+
+    function changeWord() {
+        clearInterval(flashLineListener);
+        $("#introBanner .introContainer em").css("opacity", 1);
+        if ($("#introBanner .introContainer h1").text().length > 0) {
+            deleteWord();
+        } else {
+            addWord();
+        }
+    }
+
+    // Adds a word and then pauses
+    function addWord() {
+        var wordEl = $("#introBanner .introContainer h1");
+        if (wordEl.text().length < words[currWord].length) {
+            wordEl.text(words[currWord].slice(0, wordEl.text().length+1));
+            setTimeout(addWord, 100);
+        } else {
+            currWord = (currWord + 1) % words.length;
+            flashLineListener = setInterval(flashLine, 600);
+            setTimeout(changeWord, 3000);
+        }
+    }
+
+    // Removes a word and once word removed, starts add
+    function deleteWord() {
+        var wordEl = $("#introBanner .introContainer h1");
+        if (wordEl.text().length > 0) {
+            wordEl.text(wordEl.text().slice(0, wordEl.text().length-1));
+            setTimeout(deleteWord, 100);
+        } else {
+            setTimeout(addWord, 100);
+        }
+    }
+
+
+
+})
